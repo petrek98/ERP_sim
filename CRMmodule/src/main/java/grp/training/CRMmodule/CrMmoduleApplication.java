677dc4b5266836a4d066e311c43c52c765c9f80f
mKurtycz <michall.kurtycz@gmail.com>
2023-03-02 12:39:51 +0100

    ERP-8 Project structure

---
+++ CRMmodule/src/main/java/grp/training/CRMmodule/CrMmoduleApplication.java
@@ -0,0 +1,13 @@
+package grp.training.CRMmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrMmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrMmoduleApplication.class, args);
+	}
+
+}
